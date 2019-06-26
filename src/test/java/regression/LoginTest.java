83b4b48b296fd2bc804ef2e0a896d0fad4d958aa
horobets <40602367+horobets@users.noreply.github.com>
2019-06-26 21:58:38 +0300

    added credentialsstorage

--- src/test/java/regression/LoginTest.java
+++ src/test/java/regression/LoginTest.java
@@ -1,8 +1,12 @@
 package regression;
 
 import com.trello.ui.core.BrowserFactory;
+import com.trello.ui.core.credentialsstorage.Credentials;
+import com.trello.ui.core.credentialsstorage.CredentialsStorage;
 import com.trello.ui.pages.BoardsPage;
 import com.trello.ui.pages.LoginPage;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 /**
@@ -10,13 +14,27 @@ import org.testng.annotations.Test;
  */
 public class LoginTest extends BrowserFactory {
 
+
+    protected final String credentialsStorageFilePath = "c:\\credentials\\trellocredentials.txt";
+
     LoginPage loginPage = new LoginPage();
     BoardsPage boardsPage = new BoardsPage();
 
-    @Test
-    public void login() throws InterruptedException {
+    @Parameters({"username", "password"})
+    @Test(description = "Test trello login screen", priority = 1/*, dataProvider = "TestDataProvider"-*/)
+    public void login(@Optional("") String username,
+                      @Optional("") String password) {
+
+        //use credentialsstorage if no credentials provided
+        if (username.isEmpty() || password.isEmpty()) {
+            Credentials trelloCredentials = (new CredentialsStorage(credentialsStorageFilePath)).getLastCredentials();
+            username = trelloCredentials.getUsername();
+            password = trelloCredentials.getPassword();
+        }
+
+
         loginPage.open();
-        loginPage.login("loliktestintegration@gmail.com", "iLoveBieber");
+        loginPage.login(username, password);
         boardsPage.openBoard("jacksparrowtitle");
     }
 
