2f5c4380366b1df7514c62d1112e82b50ca4352c
horobets <40602367+horobets@users.noreply.github.com>
2019-07-03 09:33:31 +0300

    lesson allure

--- src/test/java/junk/TryBrowser.java
+++ src/test/java/junk/TryBrowser.java
@@ -1,11 +1,17 @@
 package junk;
 
 import com.trello.ui.core.BrowserFactory;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.testng.annotations.Test;
 
 /**
  * Created by horobets on Jun 20, 2019
  */
+@Epic("")
+@Feature("")
+@Story("JACK")
 public class TryBrowser extends BrowserFactory {
 
 
