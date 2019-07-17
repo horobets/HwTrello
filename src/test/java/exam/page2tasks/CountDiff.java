d94650bbd5dfbe5795e4661b58edca8c76d05dc8
horobets <40602367+horobets@users.noreply.github.com>
2019-07-17 18:23:21 +0300

    trello

--- src/test/java/exam/page2tasks/CountDiff.java
+++ src/test/java/exam/page2tasks/CountDiff.java
@@ -2,6 +2,9 @@ package exam.page2tasks;
 
 import org.testng.Assert;
 
+import java.util.HashSet;
+import java.util.Set;
+
 //Найти количество различных элементов массива. Пример: для 1 4 5 1 1 3 ответ 4.
 public class CountDiff {
     public static void main(String[] args) {
@@ -14,12 +17,12 @@ public class CountDiff {
     }
 
     public static int countDiff(int[] arr) {
-        int diffCount = 0;
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] != arr[i + 1]) {
-                diffCount++;
-            }
+
+        Set<Integer> uniqVals = new HashSet<>();//use HashSet to store unique values only
+
+        for (int i = 0; i < arr.length; i++) {
+            uniqVals.add(arr[i]);
         }
-        return diffCount;
+        return uniqVals.size();
     }
 }
